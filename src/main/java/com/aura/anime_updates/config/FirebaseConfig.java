2ca0c38c2b2377165aec1fb12584d5ad18e8d398
Kirra <ap.freak.305@gmail.com>
2025-08-21 15:57:42 +0630

    [MOD] Add ENV VARS for firebase config

--- src/main/java/com/aura/anime_updates/config/FirebaseConfig.java
+++ src/main/java/com/aura/anime_updates/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -12,10 +13,13 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${fcm.service-account-key-path}")
+    private String keyPath;
+
     @PostConstruct
     public void init() throws IOException {
         FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebaseServiceAccount.json");
+                new FileInputStream(keyPath);
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))

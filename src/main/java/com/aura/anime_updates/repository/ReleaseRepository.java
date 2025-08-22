6dabdeec7f204027ff7948a1d4aa9630231621b3
Thet Htoo Zaw <thethtoozaw@blue-stone.net>
2025-08-22 11:15:18 +0630

    [RFA] cap in query

--- src/main/java/com/aura/anime_updates/repository/ReleaseRepository.java
+++ src/main/java/com/aura/anime_updates/repository/ReleaseRepository.java
@@ -11,20 +11,20 @@ public interface ReleaseRepository extends JpaRepository<Release, Long> {
     boolean existsByDownloadLink(String downloadLink);
 
     @Query(value = """
-    select 
-        r.id as releaseId, 
-        r.anime_shows_id as animeShowId,
-        ans.title as showTitle,
-        r.download_link as releaseDownloadLink,
-        r.episode as episode,
-        r.file_name as fileName,
-        ans.image_url as imgUrl,
-        false as tracked
-    from releases r
-    join anime_shows ans on ans.id = r.anime_shows_id
-    order by r.created_at desc
+    SELECT 
+        r.id AS releaseId, 
+        r.anime_shows_id AS animeShowId,
+        ans.title AS showTitle,
+        r.download_link AS releaseDownloadLink,
+        r.episode AS episode,
+        r.file_name AS fileName,
+        ans.image_url AS imgUrl,
+        false As tracked
+    FROM releases r
+    JOIN anime_shows ans ON ans.id = r.anime_shows_id
+    ORDER BY r.created_at DESC 
     """,
-            countQuery = "select count(*) from releases r",
+            countQuery = "SELECT COUNT(*) FROM releases r",
             nativeQuery = true)
     Page<ReleaseInfoResponse> getReleaseInfo(Pageable pageable);
 

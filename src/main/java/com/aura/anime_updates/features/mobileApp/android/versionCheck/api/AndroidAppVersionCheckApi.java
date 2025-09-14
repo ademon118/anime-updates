cabb7e7421cc71a66f2ee728bcd2e132cf608858
Kirra <ap.freak.305@gmail.com>
2025-09-14 14:10:15 +0630

    [FIX] Fix version check API endpoint to include /api/

--- src/main/java/com/aura/anime_updates/features/mobileApp/android/versionCheck/api/AndroidAppVersionCheckApi.java
+++ src/main/java/com/aura/anime_updates/features/mobileApp/android/versionCheck/api/AndroidAppVersionCheckApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/android/app-version")
+@RequestMapping("/api/android/app-version")
 @RequiredArgsConstructor
 public class AndroidAppVersionCheckApi {
 

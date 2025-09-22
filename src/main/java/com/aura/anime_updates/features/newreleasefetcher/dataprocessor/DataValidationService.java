67b4c5f0e4f01af133798d771ab4a92ebc9fdcad
Kirra <ap.freak.305@gmail.com>
2025-09-22 12:43:59 +0630

    [FIX] Fixed validation for new versioned episode release, and old release being re entered

--- src/main/java/com/aura/anime_updates/features/newreleasefetcher/dataprocessor/DataValidationService.java
+++ src/main/java/com/aura/anime_updates/features/newreleasefetcher/dataprocessor/DataValidationService.java
@@ -1,5 +1,6 @@
 package com.aura.anime_updates.features.newreleasefetcher.dataprocessor;
 
+import com.aura.anime_updates.features.newreleasefetcher.dataprocessor.utilities.DataProcessingUtils;
 import com.aura.anime_updates.features.newreleasefetcher.dto.RSSEntry;
 import com.aura.anime_updates.features.release.domain.repository.ReleaseRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 public class DataValidationService {
 
     private final ReleaseRepository releaseRepository;
+    private final DataProcessingUtils dataProcessingUtils;
 
     private boolean validateRSSEntryIntegrity(RSSEntry entry) {
         return entry.title() != null &&
@@ -21,19 +23,28 @@ public class DataValidationService {
                 entry.publishedDate() != null;
     }
 
+    private boolean validateNonNewEntry(RSSEntry entry) {
+        return !releaseRepository.existsByFileName(entry.title());
+    }
+
+    private boolean validateDeprecatedEntry(RSSEntry entry) {
+        return !releaseRepository.newerVersionExists(
+                dataProcessingUtils.getAnimeShowTitleFromCategory(entry.category()),
+                dataProcessingUtils.getEpisodeFromRawTitle(entry.title()),
+                dataProcessingUtils.getReleaseVersionFromRawTitle(entry.title())
+        );
+    }
+
+
     private List<RSSEntry> filterInvalidEntries(List<RSSEntry> entries) {
         return entries.stream()
                 .filter(this::validateRSSEntryIntegrity)
-                .toList();
-    }
-
-    private List<RSSEntry> filterNonNewEntries(List<RSSEntry> entries) {
-        return entries.stream()
-                .filter(entry -> !releaseRepository.existsByFileName(entry.title()))
+                .filter(this::validateNonNewEntry)
+                .filter(this::validateDeprecatedEntry)
                 .toList();
     }
 
     public List<RSSEntry> validateAndFilterRSSEntries(List<RSSEntry> entries) {
-        return filterNonNewEntries(filterInvalidEntries(entries));
+        return filterInvalidEntries(entries);
     }
 }

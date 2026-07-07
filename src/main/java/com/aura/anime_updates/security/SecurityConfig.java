05a871e7cd12c67ffa76b0558156bc024d6e896c
Kirragami <ap.freak.305@gmail.com>
2026-07-07 15:30:41 +0630

    Fix websocket endpoint

--- src/main/java/com/aura/anime_updates/security/SecurityConfig.java
+++ src/main/java/com/aura/anime_updates/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/anime-show/**").permitAll()
                         .requestMatchers("/api/android/app-version/**").permitAll()
                         .requestMatchers("/api/test/**").permitAll()
-                        .requestMatchers("/ws-sync/**").permitAll()
+                        .requestMatchers("/ws-sync/**", "/ws-party", "/ws-party/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/api-docs", "/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
